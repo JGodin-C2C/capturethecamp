3e01a705e6abc256848d55f4b867028a57a9ef0e
Julien Godin <julien.godin@camptocamp.com>
2023-01-27 17:56:57 +0100

    feat: Add a kubernetes challenge
    Signed-off-by: Julien Godin <julien.godin@camptocamp.com>

--- challenges/php-command-injection/launch.sh
+++ challenges/php-command-injection/launch.sh
@@ -2,8 +2,8 @@
 #docker run -v $(pwd)/site:/var/www/html/ -v $(pwd)/flag:/flag  -p 8080:80 -it php-command-injection
 
 #===== create first challenge
-kubectl apply -n ctf -f Deployment.yaml
-kubectl create -n ctf configmap  phpinjectionchallenge --from-file=./site/
-kubectl create -n ctf secret generic phpinjectionflag --from-file=flag=flag/flag.txt
+kubectl apply -n ctfinfra -f Deployment.yaml
+kubectl create -n ctfinfra configmap  phpinjectionchallenge --from-file=./site/
+kubectl create -n ctfinfra secret generic phpinjectionflag --from-file=flag=flag/flag.txt
 
 
---
+++ challenges/secret-invasion/launch.sh
@@ -0,0 +1,8 @@
+#/usr/bin/bash
+#docker run -v $(pwd)/site:/var/www/html/ -v $(pwd)/flag:/flag  -p 8080:80 -it php-command-injection
+
+#===== create first challenge
+kubectl apply -n ctfinfra -f secret.yaml
+kubectl create serviceaccount -n ctfinfra ctfaccount
+
+
