6cac29545df49a4a641c879d1afa1e646ed336c6
Julien Godin <julien.godin@camptocamp.com>
2023-02-14 14:58:14 +0100

    feat: fix the secret invasion
    Signed-off-by: Julien Godin <julien.godin@camptocamp.com>

--- challenges/secret-invasion/launch.sh
+++ challenges/secret-invasion/launch.sh
@@ -3,7 +3,6 @@
 
 #===== create first challenge
 kubectl apply -n ctfinfra -f secret.yaml
-kubectl create serviceaccount -n ctfinfra ctfaccount
 kubectl apply -n ctfinfra -f role.yaml
 kubectl apply -n ctfinfra -f rolebinding.yaml
 
