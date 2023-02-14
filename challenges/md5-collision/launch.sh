6d93cefbcc1c746cbc8df1e0fc9520a4c5c387c3
Julien Godin <julien.godin@camptocamp.com>
2023-02-14 09:37:48 +0100

    feat: deploy command line injection challenge
    Signed-off-by: Julien Godin <julien.godin@camptocamp.com>

--- challenges/md5-collision/launch.sh
+++ challenges/md5-collision/launch.sh
@@ -2,7 +2,7 @@
 #docker run -v $(pwd)/site:/var/www/html/ -v $(pwd)/flag:/flag  -p 8080:80 -it php-command-injection
 
 #===== create first challenge
-kubectl apply -n ctf -f Deployment.yaml
+helm -n ctf install md5collision ../../chart/challenge/ -f ./values.yaml
 kubectl create -n ctf configmap  md5collisionchallenge --from-file=./site/
 kubectl create -n ctf secret generic md5collisionflag --from-file=flag=flag/flag.txt
 
--- challenges/php-command-injection/launch.sh
+++ challenges/php-command-injection/launch.sh
@@ -2,7 +2,8 @@
 #docker run -v $(pwd)/site:/var/www/html/ -v $(pwd)/flag:/flag  -p 8080:80 -it php-command-injection
 
 #===== create first challenge
-kubectl apply -n ctfinfra -f Deployment.yaml
+kubectl create namespace ctfinfra
+helm install -n ctfinfra phpinjection ../../chart/challenge/ -f ./values.yaml
 kubectl create -n ctfinfra configmap  phpinjectionchallenge --from-file=./site/
 kubectl create -n ctfinfra secret generic phpinjectionflag --from-file=flag=flag/flag.txt
 
